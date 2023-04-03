6172c576a0d25ea26c21de3b030f411988758b3b
Tivlas <tima021003tima@gmail.com>
2023-04-03 15:29:34 +0300

    Классы для запроса пароля и email у пользователя

---
+++ FinanceManagementApp/Lab2/Helpers/EmailConsoleEntering.cs
@@ -0,0 +1,30 @@
+﻿using Lab2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Helpers
+{
+    public static class EmailConsoleEntering
+    {
+        public static string GetEmail()
+        {
+            string? email;
+            while (true)
+            {
+                Console.Write("Enter email: ");
+                email = Console.ReadLine();
+                if (!EmailValidator.IsValidEmail(email))
+                {
+                    ColorPrinter.Print(ConsoleColor.Red, "Invalid email format!");
+                    continue;
+                }
+                break;
+            }
+            return email!.Trim();
+        }
+    }
+}
---
+++ FinanceManagementApp/Lab2/Helpers/PasswordConsoleEntering.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Helpers
+{
+    public static class PasswordConsoleEntering
+    {
+        public static string GetPassword(string message)
+        {
+            ConsoleKey key;
+            string password = string.Empty;
+            while (true)
+            {
+                Console.Write($"{message}: ");
+                do
+                {
+                    var keyInfo = Console.ReadKey(intercept: true);
+                    key = keyInfo.Key;
+
+                    if (key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        Console.Write("\b \b");
+                        password = password[0..^1];
+                    }
+                    else if (!char.IsControl(keyInfo.KeyChar))
+                    {
+                        Console.Write("*");
+                        password += keyInfo.KeyChar;
+                    }
+                } while (key != ConsoleKey.Enter);
+                if (password.Length < 8)
+                {
+                    ColorPrinter.Print(ConsoleColor.Red, "Password must be at least 8 characters long!");
+                    password = string.Empty;
+                    continue;
+                }
+                Console.WriteLine();
+                break;
+            }
+            return password;
+        }
+    }
+}
