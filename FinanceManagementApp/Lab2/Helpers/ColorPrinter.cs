6accf906310bcf0eefa1870accc2a39ca8c030da
Tivlas <tima021003tima@gmail.com>
2023-04-03 15:30:09 +0300

    Класс для вывода цветного текста на экран

---
+++ FinanceManagementApp/Lab2/Helpers/ColorPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Helpers
+{
+    public static class ColorPrinter
+    {
+        public static void Print(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
