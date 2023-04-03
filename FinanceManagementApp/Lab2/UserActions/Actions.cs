3fe2ac649a0b175cb7b719d2ba6845b780212c94
Tivlas <tima021003tima@gmail.com>
2023-04-03 15:31:45 +0300

    Класс для обработки запросов пользователя
    Хранит словарь методов (запросов) и их кодов

---
+++ FinanceManagementApp/Lab2/UserActions/Actions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.UserActions
+{
+    public static class Actions
+    {
+        public static Dictionary<int, Action> UserActions = new Dictionary<int, Action>();
+
+        static Actions()
+        {
+            UserActions[3] = AddSimpleAccount;
+        }
+
+        private static (int Balance, string CurrencyName,int UserId, int CardId) AddAccount()
+        {
+            // TODO: дописать возвращаемый тип, решить как связывать карту и счет
+            throw new NotImplementedException();
+        }
+
+        private static void AddSimpleAccount()
+        {
+
+        }
+    }
+}
