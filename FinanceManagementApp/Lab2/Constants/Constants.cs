6bee71b3668bb020108a5b2c361e982f1a2e6620
Tivlas <tima021003tima@gmail.com>
2023-04-03 15:30:58 +0300

    Класс для хранения констант

---
+++ FinanceManagementApp/Lab2/Constants/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Constants
+{
+    public static class Constants
+    {
+        public static string HelpInfo => "Enter 3 to add simple account\r\nEnter 4 to add deposit\r\nEnter 5 to add loan\r\nEnter 6 to add card\r\nEnter 7 to add transaction category\r\nEnter 8 to add simple transaction\r\nEnter 9 to add transfer\r\nEnter 10 to remove category\r\nEnter 11 to remove transaction\r\nEnter 12 to remove account or card";
+    }
+}
