372ae75344f4e33ca5fe2a4cb9c5ade099bc46d3
Tivlas <tima021003tima@gmail.com>
2023-04-03 23:48:41 +0300

    Реализовал некоторые методы интерфейса

--- FinanceManagementApp/Lab2/DataBaseEmulation/Storage.cs
+++ FinanceManagementApp/Lab2/DataBaseEmulation/Storage.cs
@@ -8,6 +8,7 @@ namespace Lab2.DataBaseEmulation
 {
     public class Storage : IDataManager
     {
+        #region Containers
         public List<User>? Users { get; set; } = new List<User>();
 
         public List<IBankEntity>? BankEntities { get; set; } = new List<IBankEntity>();
@@ -15,34 +16,18 @@ namespace Lab2.DataBaseEmulation
         public List<TransactionCategory>? Categories { get; set; } = new List<TransactionCategory>();
 
         public List<TransactionBase>? MadeTransactions { get; set; } = new List<TransactionBase>();
+        #endregion
 
-        public bool AddAccountIfNotExist(int userId, AccountBase account)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AddTransaction(int userId, TransactionBase transaction)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AddTransactionCategory(int userId, TransactionCategory category)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AddUser(User user)
-        {
-            Users?.Add(user);
-        }
-
+        #region Email and password related methods
         public bool EmailPasswordMatch(string email, string password)
         {
             email = StringHasher.GetHash(email);
             password = StringHasher.GetHash(password);
             return Users is not null && Users.Any(u => u.Email == email && u.Password == password);
         }
+        #endregion
 
+        #region Statistics related methods
         public IEnumerable<TransactionBase> GetTransactionsForTimePeriod(int userId)
         {
             throw new NotImplementedException();
@@ -57,10 +42,69 @@ namespace Lab2.DataBaseEmulation
         {
             throw new NotImplementedException();
         }
+        #endregion
+
+        #region Account related methods
+        public void AddAccount(AccountBase account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAccount(int userId, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AccountExists(string name, int userId)
+        {
+            return BankEntities is not null && BankEntities.Any(e => e.Name == name && e.UserId == userId);
+        }
+        #endregion
+
+        #region Transaction related methods
+        public void AddTransaction(TransactionBase transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void RemoveTransaction(int transactionId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region TransactionCategory related methods
+        public void AddTransactionCategory(TransactionCategory category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTransactionCategory(string name, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TransactionCategoryExists(string name, int userId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region User related methods
+        public bool UserExists(string email)
+        {
+            email = StringHasher.GetHash(email);
+            return Users is not null && Users.Any(u => u.Email == email);
+        }
+        public void AddUser(User user)
+        {
+            Users?.Add(user);
+        }
 
         public int GetUserId(string email)
         {
-            if(Users is null)
+            if (Users is null)
             {
                 return -1;
             }
@@ -68,26 +112,6 @@ namespace Lab2.DataBaseEmulation
             int i = Users.FindIndex(u => u.Email == email);
             return i == -1 ? i : Users[i].Id;
         }
-
-        public void RemoveAccount(int userId, int accountId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveTransaction(int userId, int transactionId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveTransactionCategory(int userId, int categoryId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool UserExists(string email)
-        {
-            email = StringHasher.GetHash(email);
-            return Users is not null && Users.Any(u => u.Email == email);
-        }
+        #endregion
     }
 }
