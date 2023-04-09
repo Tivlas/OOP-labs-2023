8b43c9c433b13b95aac58762293a478d90afbf71
Tivlas <tima021003tima@gmail.com>
2023-04-09 19:24:07 +0300

    Правки
    Добавил метод получения имени. Новый формат вывода информации об объектах. Добавил метод добавления карты.

--- FinanceManagementApp/Lab2/UserActions/Actions.cs
+++ FinanceManagementApp/Lab2/UserActions/Actions.cs
@@ -1,4 +1,5 @@
 ﻿using FinanceManagementAppCore.Accounts;
+using FinanceManagementAppCore.Cards;
 using Lab2.DataBaseEmulation;
 using Lab2.Helpers;
 using System;
@@ -19,16 +20,47 @@ namespace Lab2.UserActions
         {
             UserActions[3] = AddSimpleAccount;
             UserActions[13] = ListBankEntities;
+            UserActions[6] = AddCard;
         }
 
-
-        private static (decimal Balance, string CurrencyName, string Name) AddAccount(int userId, Storage storage)
+        private static string GetName(int userId, Storage storage)
         {
+            string? name;
+            while (true)
+            {
+                Console.Write("Enter name: ");
+                name = Console.ReadLine();
+                if (name == "CANCEL")
+                {
+                    return name;
+                }
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    ColorPrinter.Print(ConsoleColor.Red, "At least 1 character!");
+                    continue;
+                }
+                if (storage.BankEntityExists(name, userId))
+                {
+                    ColorPrinter.Print(ConsoleColor.Red, "Account with this relatedAccName already exists!");
+                    continue;
+                }
+                break;
+            }
+            return name;
+        }
+
+        private static (bool Canceled, decimal Balance, string CurrencyName, string Name) GetGeneralArgs(int userId, Storage storage)
+        {
+            var canceled = (true, 0, "", "");
             decimal balance;
             while (true)
             {
                 Console.Write("Enter balance: ");
                 string? temp = Console.ReadLine();
+                if (temp == "CANCEL")
+                {
+                    return canceled;
+                }
                 if (!decimal.TryParse(temp, out balance))
                 {
                     ColorPrinter.Print(ConsoleColor.Red, "Invalid input!");
@@ -42,6 +74,10 @@ namespace Lab2.UserActions
             {
                 Console.Write("Enter currency name: ");
                 currencyName = Console.ReadLine();
+                if (currencyName == "CANCEL")
+                {
+                    return canceled;
+                }
                 if (currencyName is null || currencyName?.Length < 1)
                 {
                     ColorPrinter.Print(ConsoleColor.Red, "Invalid input!");
@@ -50,32 +86,54 @@ namespace Lab2.UserActions
                 break;
             }
 
-            string? name;
-            while (true)
+            string name = GetName(userId, storage);
+            if(name == "CANCEL")
             {
-                Console.Write("Enter account name: ");
-                name = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(name))
-                {
-                    ColorPrinter.Print(ConsoleColor.Red, "At least 1 character!");
-                    continue;
-                }
-                if (storage.AccountExists(name, userId))
-                {
-                    ColorPrinter.Print(ConsoleColor.Red, "Account with this name already exists!");
-                    continue;
-                }
-                break;
+                return canceled;
             }
 
-            return (balance, currencyName!, name);
+            return (false, balance, currencyName!, name);
         }
 
         public static void AddSimpleAccount(int userId, Storage storage)
         {
-            var args = AddAccount(userId, storage);
+            var args = GetGeneralArgs(userId, storage);
+            if (args.Canceled == true)
+            {
+                return;
+            }
             var acc = new SimpleAccount(args.Balance, args.CurrencyName, args.Name, userId);
-            storage.AddAccount(acc);
+            storage.AddBankEntity(acc);
+        }
+
+        public static void AddCard(int userId, Storage storage)
+        {
+            string name = GetName(userId, storage);
+            decimal balance;
+            string currencyName;
+            int accountId;
+            while (true)
+            {
+                Console.Write("Enter related account name: ");
+                string? relatedAccName = Console.ReadLine();
+                if (relatedAccName == "CANCEL")
+                {
+                    return;
+                }
+                var acc = storage?.BankEntities?.FirstOrDefault(be => be.Name == relatedAccName);
+                if(acc is null)
+                {
+                    ColorPrinter.Print(ConsoleColor.Red, "No such account!");
+                    continue;
+                }
+                accountId = acc.Id;
+                balance = acc.Balance;
+                currencyName = acc.CurrencyName;
+                break;
+            }
+
+            var card = new Card(accountId, name, balance, currencyName);
+            storage?.AddBankEntity(card);
         }
 
         public static void ListBankEntities(int userId, Storage storage)
@@ -84,10 +142,16 @@ namespace Lab2.UserActions
 
             if (bankEntities is not null)
             {
-                foreach (var be in bankEntities.Select((value, index) => new { index, value.Name, value.Id }))
+                foreach (var be in bankEntities.Select((value, index) => new { index, value}))
                 {
                     ColorPrinter.Print(ConsoleColor.Yellow, $"{be.index + 1}. ", false);
-                    Console.WriteLine($"Name: {be.Name}, id: {be.Id}");
+                    var infoList = be.value.GetInfo();
+                    List<string> temp = new();
+                    foreach(var infoItem in infoList)
+                    {
+                        temp.Add($"{infoItem.PropName}: {infoItem.propValue}");
+                    }
+                    Console.WriteLine(string.Join(", ", temp));
                 }
             }
             else
