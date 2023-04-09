f2564d0933b4381d919cc7ed9d9b6419e7376cc8
Tivlas <tima021003tima@gmail.com>
2023-04-09 22:46:59 +0300

    Добавил новые действия в строку HelpInfo

--- FinanceManagementApp/Lab2/Constants/Constants.cs
+++ FinanceManagementApp/Lab2/Constants/Constants.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab2.Constants
+﻿namespace Lab2.Const
 {
     public static class Constants
     {
-        public static string HelpInfo => 
-            $"Enter {3,2} to add simple account\r\n" +
+        public static string HelpInfo =>
+            $"Enter {3,2} to add simple account\r\n" + // done
             $"Enter {4,2} to add deposit\r\n" +
             $"Enter {5,2} to add loan\r\n" +
             $"Enter {6,2} to add card\r\n" +
@@ -19,7 +13,8 @@ namespace Lab2.Constants
             "Enter 10 to remove category\r\n" +
             "Enter 11 to remove transaction\r\n" +
             "Enter 12 to remove account or card\r\n" +
-            "Enter 13 to list ur accounts and cards";
+            "Enter 13 to list ur accounts and cards\r\n"+
+            "Enter 14 to list ur transaction categories"; // done
 
         public static string Delimiter => "———————————————————————————————————————";
     }
