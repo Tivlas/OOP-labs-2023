7697a63ff0e123619aebea9fd3603cec7ef9ee74
Tivlas <tima021003tima@gmail.com>
2023-04-11 21:01:06 +0300

    Добавил несколько методов, изменил параметр на Func<'type',bool>

--- FinanceManagementApp/FinanceManagementAppCore/Interfaces/IDataManager.cs
+++ FinanceManagementApp/FinanceManagementAppCore/Interfaces/IDataManager.cs
@@ -1,5 +1,4 @@
-﻿using FinanceManagementAppCore.Accounts;
-using FinanceManagementAppCore.Transactions;
+﻿using FinanceManagementAppCore.Transactions;
 
 namespace FinanceManagementAppCore.Interfaces
 {
@@ -15,24 +14,30 @@ namespace FinanceManagementAppCore.Interfaces
 
         bool EmailPasswordMatch(string email, string password);
 
-        int GetUserId(string email);
+        int GetUserId(Func<User, bool> match);
 
-        bool BankEntityExists(string name, int userId);
+        bool BankEntityExists(Func<IBankEntity, bool> match);
 
-        bool UserExists(string email);
+        bool UserExists(Func<User, bool> match);
 
         void AddTransactionCategory(TransactionCategory category);
 
-        void RemoveTransactionCategory(string name, int userId);
+        void RemoveTransactionCategory(Func<TransactionCategory, bool> filter);
 
-        bool TransactionCategoryExists(string name, int userId);
+        bool TransactionCategoryExists(Func<TransactionCategory, bool> filter);
 
         void AddTransaction(TransactionBase transaction);
 
-        void RemoveTransaction(int transactionId);
+        void RemoveTransaction(Func<TransactionBase, bool> filter);
 
         void AddBankEntity(IBankEntity account);
 
-        void RemoveBankEntity(int userId, string name);
+        IBankEntity? FirstOrDefaultBankEntity(Func<IBankEntity, bool> filter);
+
+        IEnumerable<IBankEntity>? GetBankEntities(Func<IBankEntity, bool> filter);
+
+        IEnumerable<TransactionBase>? GetTransactions(Func<TransactionBase, bool> filter);
+
+        IEnumerable<TransactionCategory>? GetCategories(Func<TransactionCategory, bool> filter);
     }
 }
