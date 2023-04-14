90e48b7e489be53d4a61311e59dde584e3b74c7c
Tivlas <tima021003tima@gmail.com>
2023-04-14 21:20:34 +0300

    Симулятор базы данных для консольного приложения

--- FinanceManagementApp/Persistence/Data/DbEmulatorContext.cs
+++ FinanceManagementApp/Persistence/Data/DbEmulatorContext.cs
@@ -1,24 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Abstractions;
-using Domain.Cards;
+﻿using Domain.Cards;
 using Domain.Entities.Accounts;
 using Domain.Entities.Transactions;
 
 namespace Persistence.Data;
 public class DbEmulatorContext
 {
-    List<SimpleAccount> SimpleAccounts { get; set; } = new();
-    List<Card> Cards { get; set; } = new();
-    List<SimpleTransaction> SimpleTransactions { get; set; } = new();
-    List<Transfer> Transfers { get; set; } = new();
-    List<TransactionCategory> TransactionCategories { get; set; } = new();
+    private List<SimpleAccount> _simpleAccounts { get; set; } = new();
+    private List<Card> _cards { get; set; } = new();
+    private List<SimpleTransaction> _simpleTransactions { get; set; } = new();
+    private List<Transfer> _transfers { get; set; } = new();
+    private List<TransactionCategory> _transactionCategories { get; set; } = new();
+
+    private Dictionary<Type, object> _entitiesLists;
 
     public DbEmulatorContext()
     {
+        _entitiesLists = new Dictionary<Type, object>(){
+            { typeof(SimpleAccount),  _simpleAccounts},
+            { typeof(Card), _cards },
+            { typeof(SimpleTransaction), _simpleTransactions },
+            { typeof(Transfer), _transfers },
+            { typeof(TransactionCategory), _transactionCategories }
+        };
+    }
 
+    public List<T>? GetList<T>()
+    {
+        if (_entitiesLists.TryGetValue(typeof(T), out object? list))
+        {
+            return list as List<T>;
+        }
+        return null;
     }
 }
