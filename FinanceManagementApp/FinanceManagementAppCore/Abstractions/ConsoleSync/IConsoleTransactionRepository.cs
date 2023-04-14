71bf516c28bc56520b0511538d98640a1943c85f
Tivlas <tima021003tima@gmail.com>
2023-04-14 21:18:11 +0300

    IRepository & IUnitOfWork для консольного приложения

---
+++ FinanceManagementApp/FinanceManagementAppCore/Abstractions/ConsoleSync/IConsoleTransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Transactions;
+
+namespace Domain.Abstractions.ConsoleSync;
+public interface IConsoleTransactionRepository<T> : IConsoleEntityRepository<T> where T : TransactionBase
+{
+    IGrouping<DateTime, T> GetTransactionsGroupedByDate(Func<T, bool> filter);
+
+    IGrouping<DateTime, T> GetTransactionsGroupedByCategory(Func<T, bool> filter);
+
+    IEnumerable<T> GetTransactionsForTimePeriod(Func<T, bool> filter);
+}
---
+++ FinanceManagementApp/FinanceManagementAppCore/Abstractions/ConsoleSync/IConsoleUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Cards;
+using Domain.Entities.Accounts;
+using Domain.Entities.Transactions;
+
+namespace Domain.Abstractions.ConsoleSync;
+public interface IConsoleUnitOfWork
+{
+    IConsoleEntityRepository<SimpleAccount> SimpleAccountsRepository { get; }
+    IConsoleEntityRepository<Card> CardsRepository { get; }
+    IConsoleEntityRepository<SimpleTransaction> SimpleTransactionsRepository { get; }
+    IConsoleEntityRepository<Transfer> TransfersRepository { get; }
+    IConsoleEntityRepository<TransactionCategory> TransactionCategoriesRepository { get; }
+}
