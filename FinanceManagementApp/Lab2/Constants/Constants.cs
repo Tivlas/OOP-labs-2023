fb265d9e3d9422aaab15d9eec9cca487da3d2442
Tivlas <tima021003tima@gmail.com>
2023-04-15 18:47:24 +0300

    Добавил в HelpInfo новые действия

--- FinanceManagementApp/Lab2/Constants/Constants.cs
+++ FinanceManagementApp/Lab2/Constants/Constants.cs
@@ -3,18 +3,21 @@
     public static class Constants
     {
         public static readonly string HelpInfo =
-            $"Enter {3,2} to add simple account\r\n" + //done
-            $"Enter {4,2} to add card\r\n" + //done
-            $"Enter {5,2} to add transaction category\r\n" + //done
-            $"Enter {6,2} to add simple transaction\r\n" + //----------------------------
-            $"Enter {7,2} to add transfer\r\n" +//----------------------------
-            $"Enter {8,2} to remove category\r\n" + //done
-            $"Enter {9,2} to remove transaction\r\n" + //done
-            $"Enter 10 to remove account\r\n" + //done
-            $"Enter 11 to remove card\r\n" + //done
-            $"Enter 12 to list ur accounts and cards\r\n" + //done
-            $"Enter 13 to list ur transaction categories\r\n" +
-            $"Enter 14 to list ur transactions"; //done
+            $"Enter {0,2} to quit program\r\n" +
+            $"Enter {-1,2} to logout\r\n" +
+            $"Enter {3,2} to add simple account\r\n" +
+            $"Enter {4,2} to add card\r\n" +
+            $"Enter {5,2} to add transaction category\r\n" +
+            $"Enter {6,2} to add simple transaction\r\n" +
+            $"Enter {7,2} to add transfer\r\n" +
+            $"Enter {8,2} to remove category\r\n" +
+            $"Enter {9,2} to remove transaction\r\n" +
+            $"Enter 10 to remove account\r\n" +
+            $"Enter 11 to remove card\r\n" +
+            $"Enter 12 to print ur accounts\r\n" +
+            $"Enter 13 to print ur transaction categories\r\n" +
+            $"Enter 14 to print ur transactions\r\n" +
+            $"Enter 15 to print ur cards";
 
         public static string Delimiter => "———————————————————————————————————————";
 
