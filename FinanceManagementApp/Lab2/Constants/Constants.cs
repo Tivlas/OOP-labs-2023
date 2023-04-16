69ba3941555e5eb76aa792503b31ff714faffc51
Tivlas <tima021003tima@gmail.com>
2023-04-16 23:47:58 +0300

    Изменил коды команд.

--- FinanceManagementApp/Lab2/Constants/Constants.cs
+++ FinanceManagementApp/Lab2/Constants/Constants.cs
@@ -3,21 +3,23 @@
     public static class Constants
     {
         public static readonly string HelpInfo =
-            $"Enter {0,2} to quit program\r\n" +
-            $"Enter {-1,2} to logout\r\n" +
-            $"Enter {3,2} to add simple account\r\n" +
-            $"Enter {4,2} to add card\r\n" +
-            $"Enter {5,2} to add transaction category\r\n" +
-            $"Enter {6,2} to add simple transaction\r\n" +
-            $"Enter {7,2} to add transfer\r\n" +
-            $"Enter {8,2} to remove category\r\n" +
-            $"Enter {9,2} to remove transaction\r\n" +
-            $"Enter 10 to remove account\r\n" +
-            $"Enter 11 to remove card\r\n" +
-            $"Enter 12 to print ur accounts\r\n" +
-            $"Enter 13 to print ur transaction categories\r\n" +
-            $"Enter 14 to print ur transactions\r\n" +
-            $"Enter 15 to print ur cards";
+            $"Enter {0,3} to quit program\r\n" +
+            $"Enter {-1,3} to logout\r\n" +
+            $"Enter {3,3} to add simple account\r\n" +
+            $"Enter {33,3}to remove account\r\n" +
+            $"Enter {333,3} to print ur accounts\r\n" +
+            $"Enter {4,3} to add card\r\n" +
+            $"Enter {44,3}to remove card\r\n" +
+            $"Enter {444,3} to print ur cards\r\n" +
+            $"Enter {5,3} to add transaction category\r\n" +
+            $"Enter {55,3} to remove category\r\n" +
+            $"Enter {555,3} to print ur transaction categories\r\n" +
+            $"Enter {6,3} to add simple transaction\r\n" +
+            $"Enter {66,3} to remove transaction\r\n" +
+            $"Enter {666,3}to print ur transactions\r\n" +
+            $"Enter {7,3} to add transfer\r\n" +
+            $"Enter {77,3} to remove transfer\r\n" +
+            $"Enter {777,3} to print ur transfers";
 
         public static string Delimiter => "———————————————————————————————————————";
 
