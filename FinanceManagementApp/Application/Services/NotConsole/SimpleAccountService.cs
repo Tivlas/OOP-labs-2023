a088376fe8ca74ca53ddf71ac4bc1d1e12f97835
Tivlas <tima021003tima@gmail.com>
2023-04-30 16:58:56 +0300

    Сервисы для MAUI.

---
+++ FinanceManagementApp/Application/Services/NotConsole/SimpleAccountService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions.NotConsole;
+using Domain.Abstractions.NotForConsoleAsync;
+using Domain.Entities.Accounts;
+
+namespace Application.Services.NotConsole;
+public class SimpleAccountService : ISimpleAccountService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public SimpleAccountService(IUnitOfWork unit)
+    {
+        _unitOfWork = unit;
+    }
+
+    public Task<bool> Exists(Expression<Func<SimpleAccount, bool>> filter, CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.Exists(filter, cancellationToken);
+    }
+
+    public Task<IReadOnlyList<SimpleAccount>> ListAllAsync(CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.ListAllAsync(cancellationToken);
+    }
+
+    public Task<IReadOnlyList<SimpleAccount>> ListAsync(Expression<Func<SimpleAccount, bool>> filter, CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.ListAsync(filter, cancellationToken);
+    }
+
+    public Task AddAsync(SimpleAccount entity, CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.AddAsync(entity, cancellationToken);
+    }
+
+    public Task UpdateAsync(SimpleAccount entity, CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.UpdateAsync(entity, cancellationToken);
+    }
+
+    public Task DeleteAsync(SimpleAccount entity, CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.DeleteAsync(entity, cancellationToken);
+    }
+
+    public Task<SimpleAccount?> FirstOrDefaultAsync(Expression<Func<SimpleAccount, bool>> filter, CancellationToken cancellationToken = default)
+    {
+        return _unitOfWork.SimpleAccountsRepository.FirstOrDefaultAsync(filter, cancellationToken);
+    }
+}
