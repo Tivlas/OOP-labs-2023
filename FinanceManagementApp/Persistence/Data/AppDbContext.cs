c4c0827d2cee9e7cd043bc74d1ec504800c8cda3
Tivlas <tima021003tima@gmail.com>
2023-04-30 16:54:44 +0300

    Добавил EF Core контекст БД.

---
+++ FinanceManagementApp/Persistence/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Entities.Accounts;
+using Domain.Entities.Transactions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data;
+public class AppDbContext : DbContext
+{
+    public DbSet<SimpleAccount> SimpleAccounts { get; set; }
+    public DbSet<TransactionCategory> TransactionCategories { get; set; }
+    public DbSet<SimpleTransaction> SimpleTransactions { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
