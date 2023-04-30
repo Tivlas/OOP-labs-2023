f6011da5ef8d69e9876f6eba15a7a41c2321f115
Tivlas <tima021003tima@gmail.com>
2023-04-30 16:58:35 +0300

    Интерфейсы сервисов для MAUI.

---
+++ FinanceManagementApp/Application/Abstractions/NotConsole/IBaseService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Interfaces;
+
+namespace Application.Abstractions.NotConsole;
+public interface IBaseService<T> where T : IEntity
+{
+    Task<bool> Exists(Expression<Func<T, bool>> filter,
+    CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<T>> ListAllAsync(CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> filter,
+    CancellationToken cancellationToken = default);
+
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+    Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> filter, CancellationToken
+   cancellationToken = default);
+}
