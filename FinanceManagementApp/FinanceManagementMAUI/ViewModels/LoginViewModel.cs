643edab7341aca741681cd314e294c2325767d33
Tivlas <tima021003tima@gmail.com>
2023-05-04 19:56:47 +0300

    ViewModel для LoginPage.

---
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FinanceManagementMAUI.ViewModels;
+public partial class LoginViewModel : ObservableObject
+{
+    [ObservableProperty] private string _email;
+}
