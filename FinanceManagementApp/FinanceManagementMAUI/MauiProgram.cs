d2cb3be9b98f67cd4198499f824ee51734a8e9ca
Tivlas <tima021003tima@gmail.com>
2023-05-04 19:57:45 +0300

    Зарегестрировал сервисы, представления и модели представлений.

--- FinanceManagementApp/FinanceManagementMAUI/MauiProgram.cs
+++ FinanceManagementApp/FinanceManagementMAUI/MauiProgram.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Application.Abstractions.NotConsole;
+using Application.Services.NotConsole;
+using CommunityToolkit.Maui;
+using Domain.Abstractions.NotForConsoleAsync;
+using FinanceManagementMAUI.Pages;
+using FinanceManagementMAUI.ViewModels;
+using Microsoft.Extensions.Logging;
+using Persistence.Repository;
+using Persistence.UnitOfWork;
 
 namespace FinanceManagementMAUI;
 public static class MauiProgram
@@ -8,6 +16,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -17,7 +26,29 @@ public static class MauiProgram
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
+        SetupServices(builder.Services);
+        SetupPages(builder.Services);
+        SetupViewModels(builder.Services);
 
         return builder.Build();
+    }
+
+    private static void SetupServices(IServiceCollection services)
+    {
+        services.AddSingleton<IUnitOfWork, EfUnitOfWork>();
+        services.AddSingleton<ISimpleAccountService, SimpleAccountService>();
+        services.AddSingleton<ISimpleTransactionService, SimpleTransactionService>();
+        services.AddSingleton<IUserService, UserService>();
+        services.AddSingleton<ITransactionCategoryService, TransactionCategoryService>();
+    }
+
+    private static void SetupPages(IServiceCollection services)
+    {
+        services.AddSingleton<LoginPage>();
+    }
+
+    private static void SetupViewModels(IServiceCollection services)
+    {
+        services.AddSingleton<LoginViewModel>();
     }
 }
