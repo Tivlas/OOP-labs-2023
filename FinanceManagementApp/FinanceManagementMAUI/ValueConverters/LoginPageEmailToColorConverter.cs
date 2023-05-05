ef1d1793beac313f5bab8bfa3808f812fd244e2f
Tivlas <tima021003tima@gmail.com>
2023-05-05 13:53:54 +0300

    Конвертеры валидности пароля и почты в цвет.

---
+++ FinanceManagementApp/FinanceManagementMAUI/ValueConverters/LoginPageEmailToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinanceManagementMAUI.Models;
+
+namespace FinanceManagementMAUI.ValueConverters;
+public class LoginPageEmailToColorConverter : IValueConverter
+{
+    private readonly Color _valid = Colors.Black;
+    private readonly Color _invalid = Colors.Red;
+
+    public LoginPageEmailToColorConverter()
+    {
+        
+    }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if ((bool)value)
+        {
+            return _valid;
+        }
+        return _invalid;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
