e21b9723cfac3a89b5269bb4e203eb3cadffcb44
Tivlas <tima021003tima@gmail.com>
2023-05-05 15:52:41 +0300

    Интерфейс для классов хеширования.

---
+++ FinanceManagementApp/FinanceManagementMAUI/Models/IHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementMAUI.Models
+{
+    public interface IHasher
+    {
+        string Hash(string value);
+        bool Verify(string hashedValue, string originalValue);
+    }
+}
