e5c5a8dbf1cac1dc68db32636a6951b1f0f8e462
Tivlas <tima021003tima@gmail.com>
2023-05-05 16:40:30 +0300

    Интерфейс сервиса для popup-ов.

---
+++ FinanceManagementApp/FinanceManagementMAUI/Services/IPopupService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementMAUI.Services;
+public interface IPopupService
+{
+    Task Alert(string title, string message, string cancel)
+    {
+        return App.Current.MainPage.DisplayAlert(title, message, cancel);
+    }
+}
