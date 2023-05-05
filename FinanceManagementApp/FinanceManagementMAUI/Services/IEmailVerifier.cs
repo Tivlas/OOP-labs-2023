086711439239daad98a569f76fda56481c7d5ddd
Tivlas <tima021003tima@gmail.com>
2023-05-05 22:50:46 +0300

    Добавил класс для верификации email.

---
+++ FinanceManagementApp/FinanceManagementMAUI/Services/IEmailVerifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementMAUI.Services
+{
+    public interface IEmailVerifier
+    {
+        Task<bool> Verify(string email);
+    }
+}
