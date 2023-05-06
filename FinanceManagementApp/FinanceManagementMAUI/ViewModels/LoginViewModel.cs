c59b4b0ce01d4a28a9ddedbe587b05f0b4142a98
Tivlas <tima021003tima@gmail.com>
2023-05-06 19:56:08 +0300

    code cleaun up.

--- FinanceManagementApp/FinanceManagementMAUI/ViewModels/LoginViewModel.cs
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/LoginViewModel.cs
@@ -22,7 +22,7 @@ public partial class LoginViewModel : ObservableObject
     [ObservableProperty] private bool _isPasswordValid = true;
 
     public LoginViewModel(IUserService userService, IPasswordValidator passwordValidator,
-        IEmailValidator emailValidator, IPasswordHasher passwordHasher, 
+        IEmailValidator emailValidator, IPasswordHasher passwordHasher,
         IPopupService popupService, IEmailVerifier emailVerifier, IPreferencesService preferencesService)
     {
         _userService = userService;
