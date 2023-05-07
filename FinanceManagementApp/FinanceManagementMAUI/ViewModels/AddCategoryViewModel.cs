dffcf907e8e52c19e9c6ff43e38aa2a7fb2a3bcf
Tivlas <tima021003tima@gmail.com>
2023-05-07 18:10:48 +0300

    Пофиксил баг с Name == null.

--- FinanceManagementApp/FinanceManagementMAUI/ViewModels/AddCategoryViewModel.cs
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/AddCategoryViewModel.cs
@@ -8,8 +8,10 @@ using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Domain.Entities.Transactions;
+using FinanceManagementMAUI.Const;
 using FinanceManagementMAUI.Services;
 using FinanceManagementMAUI.Services.PreferencesServices;
+using Microsoft.VisualBasic;
 
 namespace FinanceManagementMAUI.ViewModels
 {
@@ -18,13 +20,15 @@ namespace FinanceManagementMAUI.ViewModels
         private readonly ITransactionCategoryService _transactionCategoryService;
         private readonly IPreferencesService _preferencesService;
         private readonly IPopupService _popupService;
+        private readonly IServiceProvider _serviceProvider;
 
         public AddCategoryViewModel(ITransactionCategoryService transactionCategoryService, IPreferencesService preferencesService,
-            IPopupService popupService)
+            IPopupService popupService, IServiceProvider serviceProvider)
         {
             _transactionCategoryService = transactionCategoryService;
             _preferencesService = preferencesService;
             _popupService = popupService;
+            _serviceProvider = serviceProvider;
         }
 
         [ObservableProperty] private string _name;
@@ -33,12 +37,18 @@ namespace FinanceManagementMAUI.ViewModels
         public async Task AddCategory()
         {
             var userId = _preferencesService.Get("id", -1);
+            Name ??= Const.Constants.NoCategory;
             if (userId != -1 && await _transactionCategoryService.FirstOrDefaultAsync(tc => tc.Name == Name) is null)
             {
                 
                 var tc = new TransactionCategory(Name, userId);
                 await _transactionCategoryService.AddAsync(tc);
                 await _transactionCategoryService.SaveChangesAsync();
+                var tcs = _serviceProvider.GetRequiredService<DisplayCategoriesViewModel>().Categories;
+                await MainThread.InvokeOnMainThreadAsync(() =>
+                {
+                    tcs.Add(tc);
+                });
                 await _popupService.ShowToast("Successfully added!", ToastDuration.Short, 14);
             }
         }
