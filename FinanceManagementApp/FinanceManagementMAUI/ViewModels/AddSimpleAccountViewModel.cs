f18b5e37f506c56b2fb79fc7f80f22e758342409
Tivlas <tima021003tima@gmail.com>
2023-05-07 21:20:25 +0300

    Написал разметку страницы добавления карты.

--- FinanceManagementApp/FinanceManagementMAUI/ViewModels/AddSimpleAccountViewModel.cs
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/AddSimpleAccountViewModel.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Abstractions.NotConsole;
 using CommunityToolkit.Mvvm.ComponentModel;
+using FinanceManagementMAUI.Services;
 
 namespace FinanceManagementMAUI.ViewModels
 {
     public partial class AddSimpleAccountViewModel : ObservableObject
     {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ISimpleAccountService _simpleAccountService;
+        private readonly IPopupService _popupService;
+
+        public AddSimpleAccountViewModel(IServiceProvider serviceProvider,ISimpleAccountService simpleAccountService,
+            IPopupService popupService)
+        {
+            _serviceProvider = serviceProvider;
+            _simpleAccountService = simpleAccountService;
+            _popupService = popupService;
+        }
+
+        [ObservableProperty] private string _name;
+        [ObservableProperty] private string _currencyName;
+        [ObservableProperty] private string _balance;
     }
 }
