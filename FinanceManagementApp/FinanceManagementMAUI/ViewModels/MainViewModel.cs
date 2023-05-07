b681d3d801a6c8304e34a3f7b75b8f109250e2f4
Tivlas <tima021003tima@gmail.com>
2023-05-07 15:44:02 +0300

    Поправил способ проверки на существование пользователя.

--- FinanceManagementApp/FinanceManagementMAUI/ViewModels/MainViewModel.cs
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Application.Abstractions.NotConsole;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using FinanceManagementMAUI.Services.PreferencesServices;
 
@@ -6,23 +7,24 @@ namespace FinanceManagementMAUI.ViewModels;
 public partial class MainViewModel : ObservableObject
 {
     private readonly IPreferencesService _preferencesService;
+    private readonly IUserService _userService;
 
     //public MainViewModel()
     //{
-        
+
     //}
 
-    public MainViewModel(IPreferencesService preferencesService)
+    public MainViewModel(IPreferencesService preferencesService, IUserService userService)
     {
         _preferencesService = preferencesService;
+        _userService = userService;
     }
 
     [RelayCommand] async Task DoCheckIfAlreadyLoggedIn() => await CheckIfAlreadyLoggedIn();
 
     private async Task CheckIfAlreadyLoggedIn()
     {
-        int id = _preferencesService.Get("id", -1);
-        if (id == -1)
+        if (await _userService.FirstOrDefaultAsync(u => u.Id == _preferencesService.Get("id", -1)) is null)
         {
             await App.Current.MainPage.DisplayAlert("No account", "You are not logged in", "Log in");
             await Shell.Current.GoToAsync("login");
