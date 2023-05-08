5fa384365774986fdaa0c89aa2aa6768552cbdc2
Tivlas <tima021003tima@gmail.com>
2023-05-08 20:27:26 +0300

    Поправил логику добавления транзакции.

--- FinanceManagementApp/FinanceManagementMAUI/ViewModels/AddSimpleTransactionViewModel.cs
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/AddSimpleTransactionViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Abstractions.NotConsole;
+using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Domain.Entities;
@@ -20,26 +21,30 @@ public partial class AddSimpleTransactionViewModel : ObservableObject, IQueryAtt
     [ObservableProperty] private bool _isIncome;
     [ObservableProperty] private string _comment;
     [ObservableProperty] private DateTime _date = DateTime.Today;
-    [ObservableProperty] private string _amoutOfMoney = "0";
+    [ObservableProperty] private string _amountOfMoney = "0";
     [ObservableProperty] private TransactionCategory _category;
     [ObservableProperty] private SimpleAccount _selectedSimpleAccount;
     private readonly ISimpleTransactionService _simpleTransactionService;
     private readonly IPopupService _popupService;
     private readonly IPreferencesService _preferencesService;
+    private readonly ISimpleAccountService _simpleAccountService;
     private readonly MutualSimpleTransactionBinding _mutualSimpleTransactionBinding;
+    private readonly MutualSimpleAccountsBinding _mutualSimpleAccountsBinding;
 
     public MutualTransactionCategoryBindings MutualTransactionCategoryBindings { get; }
 
     public AddSimpleTransactionViewModel(ISimpleTransactionService simpleTransactionService,
-        IPopupService popupService, IPreferencesService preferencesService,
+        IPopupService popupService, IPreferencesService preferencesService, ISimpleAccountService simpleAccountService,
         MutualTransactionCategoryBindings mutualTransactionCategoryBindings,
-        MutualSimpleTransactionBinding mutualSimpleTransactionBinding)
+        MutualSimpleTransactionBinding mutualSimpleTransactionBinding, MutualSimpleAccountsBinding mutualSimpleAccountsBinding)
     {
         _simpleTransactionService = simpleTransactionService;
         _popupService = popupService;
         _preferencesService = preferencesService;
+        _simpleAccountService = simpleAccountService;
         MutualTransactionCategoryBindings = mutualTransactionCategoryBindings;
         _mutualSimpleTransactionBinding = mutualSimpleTransactionBinding;
+        _mutualSimpleAccountsBinding = mutualSimpleAccountsBinding;
     }
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
@@ -55,17 +60,17 @@ public partial class AddSimpleTransactionViewModel : ObservableObject, IQueryAtt
         decimal money = 0;
         try
         {
-            money = Calculator.Calc(AmoutOfMoney);
+            money = Calculator.Calc(AmountOfMoney);
         }
         catch (Exception)
         {
             await _popupService.Alert("Wrong input", "Amount of money must be a number greater than 0 or expression (eg: 5 * 3 - 4)", "Ok");
         }
-        if(money <= 0)
+        if (money <= 0)
         {
             await _popupService.Alert("Wrong input", "Amount of money must be a number greater than 0", "Ok");
         }
-        if (Category is null)
+        else if (Category is null)
         {
             await _popupService.Alert("Error", "Select transaction category", "Ok");
         }
@@ -73,9 +78,32 @@ public partial class AddSimpleTransactionViewModel : ObservableObject, IQueryAtt
         {
             var transaction = new SimpleTransaction(SelectedSimpleAccount.Id, Date, IsIncome, money, Category,
                 Comment, _preferencesService.Get("id", -1));
+
+            int index = _mutualSimpleAccountsBinding.SimpleAccounts.IndexOf(SelectedSimpleAccount);
+
+            if (IsIncome)
+            {
+                SelectedSimpleAccount.Balance += money;
+            }
+            else
+            {
+                SelectedSimpleAccount.Balance -= money;
+            }
+
+            var accToEdit = await _simpleAccountService.FirstOrDefaultAsync(a => a.UserId == SelectedSimpleAccount.UserId &&
+            a.Name == SelectedSimpleAccount.Name);
+            if (accToEdit is not null)
+            {
+                accToEdit.Balance = SelectedSimpleAccount.Balance;
+                await _simpleAccountService.UpdateAsync(accToEdit);
+                await _simpleAccountService.SaveChangesAsync();
+            }
+
+            _mutualSimpleAccountsBinding.Edit(SelectedSimpleAccount, index);
             await _simpleTransactionService.AddAsync(transaction);
             await _simpleTransactionService.SaveChangesAsync();
             _mutualSimpleTransactionBinding.Add(transaction);
+            await _popupService.ShowToast("Successfully added", ToastDuration.Short, 14);
         }
     }
 }
--- FinanceManagementApp/FinanceManagementMAUI/ViewModels/DisplaySimpleTransactionsViewModel.cs
+++ FinanceManagementApp/FinanceManagementMAUI/ViewModels/DisplaySimpleTransactionsViewModel.cs
@@ -4,8 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using FinanceManagementMAUI.Services.Bindings;
 
 namespace FinanceManagementMAUI.ViewModels;
 public partial class DisplaySimpleTransactionsViewModel : ObservableObject
 {
+    public DisplaySimpleTransactionsViewModel(MutualSimpleTransactionBinding mutualSimpleTransactionBinding)
+    {
+        MutualSimpleTransactionBinding = mutualSimpleTransactionBinding;
+    }
+
+    public MutualSimpleTransactionBinding MutualSimpleTransactionBinding { get; }
 }
