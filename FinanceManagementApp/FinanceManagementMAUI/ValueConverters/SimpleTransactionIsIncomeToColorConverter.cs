56d758f0cd29d8d96bd8d4090ac0ac70e427a1be
Tivlas <tima021003tima@gmail.com>
2023-05-11 23:00:05 +0300

    Добавил конвертер IsIncome в цвет, изменил интерфейс.

---
+++ FinanceManagementApp/FinanceManagementMAUI/ValueConverters/SimpleTransactionIsIncomeToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementMAUI.ValueConverters
+{
+    class SimpleTransactionIsIncomeToColorConverter : IValueConverter
+    {
+        private readonly Color _income = Colors.Green;
+        private readonly Color _expense = Colors.Red;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return _income;
+            }
+            return _expense;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
