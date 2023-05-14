762a51a657bd7bd2ba4aa1e4579c6556d934d183
Tivlas <tima021003tima@gmail.com>
2023-05-14 11:15:54 +0300

    Исправил ошибку с выводом пустой строки если SimpleAccount у транзакции оказался null.

---
+++ FinanceManagementApp/FinanceManagementMAUI/ValueConverters/NullToValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.Accounts;
+
+namespace FinanceManagementMAUI.ValueConverters;
+public class NullToValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is null)
+        {
+            return parameter;
+        }
+        else
+        {
+            return (value as SimpleAccount).Name;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
