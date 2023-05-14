eb6195dfa01035a364eb77ac61bb705769fc6955
Tivlas <tima021003tima@gmail.com>
2023-05-14 11:40:33 +0300

    Конвертер аббревиатуры во флаг.

---
+++ FinanceManagementApp/FinanceManagementMAUI/ValueConverters/CurrencyAbbreviationToFlagConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementMAUI.ValueConverters;
+public class CurrencyAbbreviationToFlagConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string country = (value as string)![..2];
+        var flag = $"{string.Concat(country.ToUpper().Select(x => char.ConvertFromUtf32(x + 0x1F1A5)))} ({(value as string)!})";
+        return flag;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
